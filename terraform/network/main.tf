7d9b9dc207c374c235e5cc08dc6f973488fe6736
Vaibhavarora08 <38854840+Vaibhavarora08@users.noreply.github.com>
2020-09-16 19:05:09 +1000

    added the route in private subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -38,6 +38,10 @@ resource "aws_route_table" "mw_rt_public" {
 
 resource "aws_route_table" "mw_rt_private" {
   vpc_id = "${aws_vpc.mw_vpc.id}"
+  route{
+      cidr_block = "0.0.0.0/0"
+      instance_id = "eni"
+  }
 
   tags{
       Name = "mediawiki_rt_private"
